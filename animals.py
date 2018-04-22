b1b4d37f9909354b3223eaa8f2e00cbdeee03f0e
MartinMolinero <a01204818@itesm.mx>
2018-04-22 17:40:23 -0500

    nn

--- animals.py
+++ animals.py
@@ -9,14 +9,23 @@ tuples = predicate_matrix_file.readlines()
 classes = train_classes_file.readlines()
 #print(classes)
 
-def do_for_neural_networks():
+def do_for_neural_networks(x_array, class_numbers):
+    clf = MLPClassifier(solver='lbfgs', alpha=1e-5,hidden_layer_sizes=(5, 2), random_state=1)
+    clf.fit(x_array, class_numbers)
+    test = train_predicates.readlines()
+    t_array = []
+    for s in test:
+        s =s.rstrip()
+        s = s.split(' ')
+        t_array.append(s)
+    print(clf.predict(t_array))
     pass
 
 
 def do_for_random_forest_tree():
     pass
 
-def do_for_svm(x_array, class_numbers, test):
+def do_for_svm(x_array, class_numbers):
     clf = svm.SVC(decision_function_shape='ovr')
     print(clf.fit(x_array, class_numbers))
     test = train_predicates.readlines()
@@ -44,4 +53,5 @@ def main():
         class_numbers.append(c[0])
         class_names.append(c[1])
 
-    do_for_svm(x_array, class_numbers, test)
+    do_for_svm(x_array, class_numbers)
+    do_for_neural_networks(x_array, class_numbers)
